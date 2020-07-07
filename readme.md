54f6b41876b27f5f5d59fadeb39d09c195c8da33
Pelle Hjek <hjek@member.fsf.org>
2020-07-07 12:36:12 +0200

    Initial commit

---
+++ readme.md
@@ -0,0 +1,16 @@
+# view-as-pdf
+## Description
+A web service allowing people to fetch a pdf copy of documents from web-based text editors. May be useful if 
+- you're using a low-spec device, e.g. iPhone or a second-hand laptop
+- you don't want to run Google Docs on your device
+
+Currently supported web-based text editors:
+- Google Docs
+- Cryptpad
+- OnlyOffice
+
+By default only certain domains are allowed.
+
+## How to start a server
+- `npm install`
+- `npm start`
---
+++ server.js
@@ -0,0 +1,96 @@
+// view-as-pdf
+// Copyright (c) 2020 Pelle Hjek
+// Affero GPL version 3
+
+const puppeteer = require('puppeteer')
+const http = require('http')
+const url = require('url')
+
+// list of allowed hosts
+const allowedHosts = [
+    'example.com',
+    'docs.google.com',
+    'cryptpad.organise.earth',
+    'cryptpad.fr'
+]
+
+const port = process.env.npm_package_config_port || "8080"
+const form =
+`<!DOCTYPE html>
+<html>
+  <head>
+    <title>view as pdf</title>
+  </head>
+  <body>
+    <form target="_blank">
+      <input name="url" type="url" required="required" placeholder="https://docs.google.com/whatever"></input>
+      <input type="submit" value="view as pdf"></input>
+    </form>
+  </body>
+</html>`
+
+const urlToPdf = async (my_url) => {
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+//    page.setDefaultTimeout(600*1000) // 10 minutes timeout for testing
+    await page.goto(my_url)
+    await isLoaded(my_url, page)
+    const buffer = await page.pdf({format: 'A4', waitUntil: 'networkidle2'})
+    await browser.close()
+    return buffer
+}
+
+const isLoaded = async (my_url, page) => {
+    // check if page is done loading. requires app specific quirks.
+    if (my_url.host === 'docs.google.com') {
+    // google docs
+	await page.waitForSelector('.docs-gm')
+	return true
+    }
+    if (my_url.href.match(/\/onlyoffice\//)) {
+    // onlyoffice
+	await page.waitForSelector('.app-onlyoffice')
+	return true
+    }
+    if (my_url.href.match(/\/pad\//)) {
+    // cryptpad
+	await page.waitForSelector('.app-pad')
+	return true
+    }
+    return true
+}
+
+const isAllowed = (my_url) => {
+    return allowedHosts.includes(my_url.host)
+}
+
+http.createServer(async (req, res) => {
+    const queryObject = url.parse(req.url,true).query
+    try {
+	const my_url = new URL(queryObject.url)
+	if (isAllowed(my_url)) {
+	    await urlToPdf(my_url).then(
+		buffer => {
+		    res.writeHead(200)
+		    res.write(buffer, 'binary')
+		    res.end(null, 'binary')
+		}).catch( 
+		    error => {
+			res.writeHead(500, {'Content-Type': 'text/plain'})
+			res.write("sorry, failed to create pdf:\n\n" + error)
+			res.end(null)
+		    })
+	} else {
+	    res.writeHead(403, {'Content-Type': 'text/plain'})
+	    res.write('sorry, ' + my_url.host + ' not allowed')
+	    res.end(null)
+	}
+    } catch(e) {
+	// without valid url parameter given, just show input form
+	res.writeHead(200, {'Content-Type': 'text/html'})
+	res.write(form)
+	res.end(null)
+    }
+}).listen(port)
+
+console.log('serving on :' + port)
